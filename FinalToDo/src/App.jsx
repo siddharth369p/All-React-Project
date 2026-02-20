a92224fdea19de7de1dc72e9efd337c581bbf20c
siddharth369p <sidpathak2002@gmail.com>
2026-02-20 23:18:45 +0530

    basic

---
+++ FinalToDo/src/App.jsx
@@ -0,0 +1,45 @@
+
+import AppName from "./components/AppName"
+import AppTodo from "./components/AppTodo"
+import Todoitems from "./components/Todoitems";
+import"./App.css";
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+
+function App() {
+  
+  const [todoItems,setTodoItems]=useState([]);
+
+
+  const handleNewItem=(todoName,todoDate)=>{
+    console.log(`Newitem is added  ${todoName} and Date:${todoDate}`);
+
+    let newTodoItems=[...todoItems,{ todoName:todoName,
+    todoDate:todoDate}]
+
+    setTodoItems(newTodoItems);
+  }
+   
+  const handleDeleteitem=(todoItemName)=>{
+
+    const newTodoItem= todoItems.filter((item=>item.name===todoItemName));
+
+    setTodoItems(newTodoItem);
+  }
+
+
+  return (
+
+   <div className="todo-container">
+       <AppName/>
+        <AppTodo onNewItem={handleNewItem}/>
+        {todoItems.length===0 && <WelcomeMessage/>}
+        
+       <Todoitems todoitems={  todoItems} onDeleteClick={handleDeleteitem}/>
+   </div>
+   
+  )
+}
+
+export default App
