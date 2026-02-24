ffde7b226825b9de65367d74accd70ca47f2fe2f
siddharth369p <sidpathak2002@gmail.com>
2026-02-24 11:19:24 +0530

    fin

--- socialMedia/Social/src/store/post-list-store.jsx
+++ socialMedia/Social/src/store/post-list-store.jsx
@@ -7,15 +7,45 @@ export const PostList = createContext({
 });
 
 const postListReducer = (currPostList, action) => {
-  return currPostList;
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId,
+    );
+  } else if (action.type === "ADD_POST") {
+
+    newPostList = [action.payload, ...currPostList];
+   
+  }
+  return newPostList;
 };
 
 const PostListProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer(postListReducer, Default_Post_List);
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    Default_Post_List,
+  );
 
-  const addPost = () => {};
+  const addPost = (userId, postBody, postTitle, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reaction: reactions,
+        userid: userId,
+        tags: tags,
+      },
+    });
+  };
   const deletePost = (postId) => {
-    console.log("you click on delete post")
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
   };
 
   return (
@@ -31,24 +61,23 @@ const PostListProvider = ({ children }) => {
   );
 };
 
-
-     const Default_Post_List=[
-      {
-      id:'1',
-      title:"Vaccational Trip",
-      body:"hiii everyone i am going to tell you about my vaccation that was so amaxing",
-      reaction:"5",
-      userid:"user-9",
-      tags:["vaccation","vlog"]
-     },
-        {
-      id:'2',
-      title:"VrindaVan Trip",
-      body:"its all about  that was so amazing and so adventure things was happened",
-      reaction:"16",
-      userid:"user-9",
-      tags:["vaccation","Travelling"]
-     },
-    ]
+const Default_Post_List = [
+  {
+    id: "1",
+    title: "Vaccational Trip",
+    body: "hiii everyone i am going to tell you about my vaccation that was so amaxing",
+    reaction: "5",
+    userid: "user-9",
+    tags: ["vaccation", "vlog"],
+  },
+  {
+    id: "2",
+    title: "VrindaVan Trip",
+    body: "its all about  that was so amazing and so adventure things was happened",
+    reaction: "16",
+    userid: "user-9",
+    tags: ["vaccation", "Travelling"],
+  },
+];
 
 export default PostListProvider;
